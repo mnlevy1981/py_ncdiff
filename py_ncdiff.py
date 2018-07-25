6855aad1b002321f42c38cb0c9c33e99c29b13f1
Michael Levy <mike.levy.work@gmail.com>
2018-07-25 14:38:08 -0600

    Add first test and clean up output
    compare_variable_names() looks at all variables in baseline and new_file; pass
    implies variable names are the same in both files.
    
    Added a --quiet option that only logs test results, but by default more
    information about failures are provided.

--- py_ncdiff.py
+++ py_ncdiff.py
@@ -12,6 +12,7 @@ Besides standard libraries, needs the following python packages:
 * netCDF4
 * xarray
 """
+import logging
 
 ###################
 
@@ -23,8 +24,6 @@ def init_logging():
     which in turn is based on
     http://stackoverflow.com/a/24956305/1076493
     """
-
-    import logging
     import sys
 
     class MaxLevelFilter(logging.Filter):
@@ -63,14 +62,14 @@ class netCDF_comp_class(object):
 
     ###################
 
-    def __init__(self, file1, file2):
+    def __init__(self, baseline, new_file, quiet=False):
         """
         Use xarray to read datasets (save as class objects)
         """
         import os
         import sys
-        import logging
         import time
+        from collections import OrderedDict
 
         logger = logging.getLogger(__name__)
         try:
@@ -79,23 +78,97 @@ class netCDF_comp_class(object):
             logger.error('Can not import xarray')
             sys.exit(1)
 
-        # Read file1
-        if os.path.isfile(file1):
-            self.ds1 = xr.open_dataset(file1, decode_times=False, decode_coords=False)
+        # Set up dictionaries to store datasets / metadata about datasets and also test results
+        self.baseline = dict()
+        self.new_file = dict()
+        self.quiet = quiet
+        self.test_results = OrderedDict()
+
+        # Read baseline
+        if os.path.isfile(baseline):
+            self.baseline['ds'] = xr.open_dataset(baseline, decode_times=False, decode_coords=False)
+            self.baseline['vars'] = self.baseline['ds'].variables.keys()
         else:
-            logger.error('Can not open %s', file1)
+            logger.error('Can not open %s', baseline)
             sys.exit(1)
 
-        # Read file2
-        if os.path.isfile(file2):
-            self.ds2 = xr.open_dataset(file2, decode_times=False, decode_coords=False)
+        # Read new_file
+        if os.path.isfile(new_file):
+            self.new_file['ds'] = xr.open_dataset(new_file, decode_times=False, decode_coords=False)
+            self.new_file['vars'] = self.new_file['ds'].variables.keys()
         else:
-            logger.error('Can not open %s', file2)
+            logger.error('Can not open %s', new_file)
             sys.exit(1)
 
-        logger.info("Comparing %s and %s", file1, file2)
-        logger.info("First file modified:  %s", time.ctime(os.path.getmtime(file1)))
-        logger.info("Second file modified: %s", time.ctime(os.path.getmtime(file1)))
+        # Make list of variables that appear in both
+        self.common_vars = list(set(self.baseline['vars']) & set(self.new_file['vars']))
+
+        logger.info("Comparing %s and %s\n-----", baseline, new_file)
+        logger.info("Baseline modified: %s", time.ctime(os.path.getmtime(baseline)))
+        logger.info("New file modified: %s", time.ctime(os.path.getmtime(new_file)))
+        logger.info("-----")
+
+    ###################
+
+    def compare_variable_names(self):
+        """
+        PASS if all variables in baseline are also in new_file and vice versa
+        FAIL if any variable is in baseline but not new_file or vice versa
+        """
+        logger = logging.getLogger(__name__)
+        test_desc = 'Compare Variable Names'
+        self.test_results[test_desc] = dict()
+
+        # Lists are identical if self.baseline['vars'], self.new_file['vars'], and self.common_vars
+        # are all the same length
+        if ((len(self.baseline['vars']) == len(self.new_file['vars'])) and
+            (len(self.baseline['vars']) == len(self.common_vars))):
+            self.test_results[test_desc]["result"] = "Variable list matches -- all variables exist in both files"
+            self.test_results[test_desc]["pass"] = True
+            return
+
+        # Variable lists do not match!
+        # Log data unless run with --quiet
+        if not self.quiet:
+            # (1) Create lists of variables in one file but not the other
+            baseline_only = [var for var in self.baseline['vars'] if var not in self.common_vars]
+            new_file_only = [var for var in self.new_file['vars'] if var not in self.common_vars]
+
+            # (2) Log the contents of these lists
+            if baseline_only:
+                logger.info("%d variable(s) are in the baseline but not the new file:", len(baseline_only))
+                for n, var in enumerate(baseline_only):
+                    logger.info("%d. %s", n+1, var)
+            if new_file_only:
+                logger.info("%d variable(s) are in the new file but not the baseline:", len(new_file_only))
+                for n, var in enumerate(new_file_only):
+                    logger.info("%d. %s", n+1, var)
+            logger.info("")
+        # Store test results
+        self.test_results[test_desc]["result"] = "Variable list does not match -- some variables exist in one file but not the other"
+        self.test_results[test_desc]["pass"] = False
+
+    ###################
+
+    def parse_results(self):
+        """
+        Summarize test results
+        """
+        logger = logging.getLogger(__name__)
+        fail_cnt = 0
+        for n, test_desc in enumerate(self.test_results.keys()):
+            # Increment fail count
+            if not self.test_results[test_desc]["pass"]:
+                fail_cnt += 1
+            if self.quiet:
+                if self.test_results[test_desc]["pass"]:
+                    results = test_desc + ": PASS"
+                else:
+                    results = test_desc + ": FAIL"
+            else:
+                results = self.test_results[test_desc]["result"]
+            logger.info("(%d) %s", n+1, results)
+        return fail_cnt
 
 ###################
 
@@ -105,25 +178,39 @@ def _parse_args():
 
     import argparse
 
-    parser = argparse.ArgumentParser(description="Compare two netCDF files; rel error is (file1-file2)/file1",
+    parser = argparse.ArgumentParser(description="Compare two netCDF files; rel error is (baseline-new_file)/baseline",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('--file1', action='store', dest='file1',
-                        help='First file to compare')
+    parser.add_argument('--baseline', action='store', dest='baseline', required=True,
+                        help='Baseline (file to be compared against)')
 
-    parser.add_argument('--file2', action='store', dest='file2',
-                        help='Second file to compare')
+    parser.add_argument('--new_file', action='store', dest='new_file', required=True,
+                        help='New file (file to compare to baseline)')
+
+    parser.add_argument('-q', '--quiet', action='store_true', dest='quiet',
+                        help="Very simple output")
 
     parser.add_argument('--vars', nargs='+', action='store', dest='vars',
-                        default=None, help="Specifc variables to compare (None => compare all)")
+                        default=None, help="Specific variables to compare (None => compare all)")
 
     return parser.parse_args()
 
 ###################
 
 if __name__ == "__main__":
+    import sys
     # Parse command-line arguments (marbl_root is used to set default for JSON file location)
     args = _parse_args()
     init_logging()
+    logger = logging.getLogger(__name__)
 
-    test=netCDF_comp_class(args.file1, args.file2)
+    test=netCDF_comp_class(args.baseline, args.new_file, args.quiet)
+    same_header = test.compare_variable_names()
+
+    # Summary (requires test and error counts)
+    err_cnt = 0
+    test_cnt = 0
+    logger.info("Summary\n-------")
+    err_cnt = test.parse_results()
+
+    sys.exit(err_cnt)
